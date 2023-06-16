204e16428702e10ae88a796a0a105a995be8be06
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-06-16 10:56:06 +0700

    f

--- Stack array/Stack array.cpp
+++ Stack array/Stack array.cpp
@@ -58,6 +58,12 @@ int main() {
 	StackArray s;
 	char ch;
 	while (true) {
+		cout << endl;
+		cout << "\n***Stack Menu***\n";
+		cout << "1, Push\n";
+		cout << "2. Pop\n";
+		cout << "3. Display\n";
+		cout << "4, Exit\n";
 		cout
 	}
 }
