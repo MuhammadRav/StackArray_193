6b4045e5a1f1c1109ccce90cde8b71e09672289e
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-06-16 10:54:03 +0700

    sa

--- Stack array/Stack array.cpp
+++ Stack array/Stack array.cpp
@@ -26,4 +26,30 @@ public:
 
 		return element;
 	}
+
+	void pop() {
+		if (empty()) {		//step 1
+			cout << "\nStack is empty. Cannot pop." << endl;	//1.a
+			return;		//1.b
+		}
+
+		cout << "\nThe popped element is: " << stack_array[top] << endl;	// step 2
+		top--;	//step 3 decrement
+	}
+
+	//method for check if data is empty
+	bool empty() {
+		return (top == -1)
+	}
+
+	void display() {
+		if (empty()) {
+			cout << "\nStack is empty." << endl;
+		}
+		else {
+			for (int tmp = 0; tmp <= top; tmp++) {
+				cout << stack_array[tmp] << endl;
+			}
+		}
+	}
 };
